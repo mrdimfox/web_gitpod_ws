20f7e15fd2b0246a3e6fdc2f09da19a3cbca59c3
mrdimfox <mrlisdim@gmail.com>
2020-10-28 16:29:57 +0000

    Add sample project on VUE

---
+++ vk-vue/src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '@/views/Create.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'create',
+    component: () => import('@/views/Create.vue')
+  },
+  {
+    path: '/list',
+    name: 'list',
+    component: () => import('@/views/List.vue')
+  },
+  {
+    path: '/task/:id',
+    name: 'task',
+    component: () => import('@/views/Task.vue')
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
